013a8c608f1291a72c660be7ce8d07ef0b022ad5
subhransu das <subhransud525@gmail.com>
2020-12-27 21:24:49 +0530

    Adding master.css, base.html, app urls.py and making view funcition for showing base.html and some changes in settings.py

--- Portfolio_using_Django/settings.py
+++ Portfolio_using_Django/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR=[
+STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"staticfiles"),
 ]
 
 MEDIA_URL = '/media/'
-MEDIAFILES_DIR=[
+MEDIAFILES_DIRS=[
     os.path.join(BASE_DIR,"media"),
 ]
