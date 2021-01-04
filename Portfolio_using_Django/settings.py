b468fde92695d58aa019e4046b82a1dd3bf1f0db
subhransu das <subhransud525@gmail.com>
2021-01-04 20:34:17 +0530

    fetching data from server

--- Portfolio_using_Django/settings.py
+++ Portfolio_using_Django/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 'django.template.context_processors.media',
             ],
         },
     },
@@ -124,7 +125,5 @@ STATICFILES_DIRS=[
 ]
 
 
-MEDIA_URL = '/media/'
-MEDIAFILES_DIRS=[
-    os.path.join(BASE_DIR,"media"),
-]
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
