c0ec0cef145f34bbadaaa13b5aea9a37ef2861c6
subhransu das <subhransud525@gmail.com>
2021-04-11 18:31:11 +0530

    fixed user table from customer user

--- Portfolio/admin.py
+++ Portfolio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 from .models import (MyDetail,
                      Project,
                      Achievment,
@@ -11,8 +12,18 @@ from .models import (MyDetail,
                      Language,
                      )
 
-
 # Register your models here.
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserModifiedAdmin(UserAdmin):
+    def get_queryset(self, request):
+        qs = super(UserModifiedAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(username=request.user.username)
+
 
 @admin.register(MyDetail)
 class MyDetailAdmin(admin.ModelAdmin):
